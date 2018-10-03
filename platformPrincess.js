d0bb63298050d762e3fb0e035738ff0b24a24aeb
Rob Fink <finkr@dewv.edu>
2018-10-03 16:54:40 +0000

    Updates via sync script

--- platformPrincess.js
+++ platformPrincess.js
@@ -1,9 +1,9 @@
 import { game, Sprite } from "./sgc/sgc.js";
 
-game.setBackground("water.png", 500);
+game.setBackground("water.png", 500, 0);
 
 class wall extends Sprite {
-    constructor(){
+    constructor() {
         super();
         this.name = "wall";
         this.setImage("wall.png");
@@ -12,3 +12,23 @@ class wall extends Sprite {
         this.accelerateOnBounce = false;
     }
 }
+
+class Support extends Sprite {
+    constructor(x, y, image) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.setImage();
+    }
+}
+
+class Platform extends Support{
+    constructor(x, y, image){
+        super();
+        this.name = "A platform";
+        this.accelerateOnBounce = false;
+    }
+}
+
+new Platform (0, 400, "start.png");
+new Platform (game.displayWidth - 48 * 2, 400, "finish.png");
